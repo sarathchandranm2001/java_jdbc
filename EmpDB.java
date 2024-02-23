a9215d00bb60d22247992f7f9e78a6dcc2683d78
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2024-02-23 14:29:09 +0530

    Add new Java files for JDBC functionality

---
+++ EmpDB.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+
+public class EmpDB {
+    public static void main(String args[])
+    {
+        Connection con;
+        Statement st;
+        ResultSet rs;
+        int no;
+        String name;
+        float sal;
+        Scanner s=new Scanner(System.in);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/java?characterEncoding=utf8","root","");
+            st=con.createStatement();
+            String str="Select * from emp,department where emp.dno=department.dno;";
+            rs=st.executeQuery(str);
+            while(rs.next()){
+                System.out.println(rs.getString("eno")+" "+rs.getString("ename")+" "+""+rs.getString("esal")+" "+rs.getString("dloc"));
+            }
+            
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error"+e);
+        }
+
+    }
+}
